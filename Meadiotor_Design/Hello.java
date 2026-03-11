61ac09745da9066f373538d052355ee8338da4f0
Shambelkibr <shambel5110@gmail.com>
2026-03-11 09:45:33 +0300

    Meadiator Design by java

---
+++ Meadiotor_Design/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+    private static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
